432e21146c6a95431222bc3257ebd02b89db7ed5
Leandro Burgos <burgosleandro.c@gmail.com>
2023-04-15 19:30:55 -0400

    modified head.tsx add metas to SEO

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Leandro Burgos Corvalán</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="title" property="og:title" content="Leandro's Portfolio Site" />
+      <meta name="type" property="og:type" content="website" />
+      <meta name="image" property="og:image" content="../public/portfolioproject.png" />
+      <meta name="url" property="og:url" content="https://leaburgos.vercel.app/" />
       <meta name="description" content="Bienvenido/a a mi portafolio personal" />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </>
