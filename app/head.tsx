617292c43533804173b96f1bcc812a637a5ac1c9
Leandro Burgos <burgosleandro.c@gmail.com>
2023-04-15 20:35:59 -0400

    update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,7 @@ export default function Head() {
       <meta name="title" property="og:title" content="Leandro's Portfolio Site" />
       <meta name="type" property="og:type" content="website" />
       <meta name="url" property="og:url" content="https://leaburgos.vercel.app/" />
-      <meta property="og:image" content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZ3JhbWFjaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+      <meta name="image "property="og:image" content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZ3JhbWFjaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
       <meta name="description" content="Bienvenido/a a mi portafolio personal" />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </>
